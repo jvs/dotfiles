c0729480c84e8668e087f72c1f65485123e4efee
jvs <vonseg@protonmail.com>
2019-12-07 20:13:22 -0600

    Update setup script

--- setup-jvs-bashrc.sh
+++ setup-jvs-bashrc.sh
@@ -20,14 +20,16 @@ fi
 EOL
 
 sudo apt update
-sudo apt install default-jre git terminator
+sudo apt install curl default-jre git terminator
 
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
+sudo apt update
 sudo apt install sbt
 
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update
 sudo apt install yarn
 
 sudo snap install code --classic
