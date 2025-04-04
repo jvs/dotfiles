978f7566b5d2ac21725483a43a4a2d0577d29097
jvs <vonseg@protonmail.com>
2025-04-03 19:03:55 -0500

    Add tmux command palette. (#4)
    * Work in progress.
    
    * Cleanup.

--- bin/tmux-commands.sh
+++ bin/tmux-commands.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 if [[ "$1" == "always-attach" ]]; then
   if [[ -z "$TMUX" ]]; then
@@ -26,7 +26,7 @@ if [[ "$1" == "show-menu" ]]; then
     "Create New Window"           w "new-window -c \"#{pane_current_path}\"" \
     "Choose Window in Session"    c "choose-tree -wf\"##{==:##{session_name},#{session_name}}\"" \
     "Switch to Last Window"       l "last-window" \
-    "Open Terminal Popup"         m "run-shell '~/bin/tmux-commands.sh popup-terminal'" \
+    "Toggle Floating Terminal"    m "run-shell '~/bin/tmux-commands.sh floating-terminal'" \
     "Rename Window"               r "command-prompt -p \"Rename window:\" \"rename-window '%%'\"" \
     "Kill Current Window"         e "confirm-before -p \"Kill window?\" kill-window" \
     "" \
@@ -50,6 +50,7 @@ if [[ $1 == "choose-session" ]]; then
     | fzf \
       --reverse \
       --header jump-to-session \
+      --info=hidden \
       --preview 'tmux capture-pane -pt {}' \
       --bind 'enter:execute(tmux switch-client -t {})+accept'"
 
@@ -64,6 +65,7 @@ if [[ $1 == "kill-session" ]]; then
     | fzf \
       --reverse -m \
       --header=kill-session \
+      --info=hidden \
       --preview 'tmux capture-pane -pt {}' \
     | xargs -I {} tmux kill-session -t {}"
 
@@ -71,28 +73,30 @@ if [[ $1 == "kill-session" ]]; then
 fi
 
 
-if [[ $1 == "popup-terminal" ]]; then
+if [[ $1 == "floating-terminal" ]]; then
   current_session=$(tmux display-message -p '#S')
   current_window=$(tmux display-message -p '#W')
+  current_path=$(tmux display-message -p '#{pane_current_path}')
 
-  if [ "$current_session" = "popup-terminals" ]; then
+  if [ "$current_session" = "floating-terminals" ]; then
     tmux detach-client
     exit 0
   fi
 
-  if [ -z "$(tmux list-sessions -F '#{session_name}' | grep "^popup-terminals$")" ]; then
-    tmux new-session -d -s "popup-terminals"
+  if [ -z "$(tmux list-sessions -F '#{session_name}' | grep "^floating-terminals$")" ]; then
+    tmux new-session -d -s "floating-terminals"
   fi
 
-  popup_session="popup-terminals"
-  popup_window="$current_session--$current_window"
-  window_exists=$(tmux list-windows -t "$popup_session" -F '#{window_name}' | grep -c "^$popup_window$")
+  floating_session="floating-terminals"
+  floating_window="$current_session--$current_window"
+  window_exists=$(tmux list-windows -t "$floating_session" -F '#{window_name}' \
+    | grep -c "^$floating_window$")
 
   if [ "$window_exists" -eq 0 ]; then
-    tmux new-window -kd -n "$popup_window" -t "$popup_session" -c "#{pane_current_path}"
+    tmux new-window -kd -n "$floating_window" -t "$floating_session" -c "$current_path"
   fi
 
-  tmux display-popup -h 80% -w 80% -E "tmux attach-session -t $popup_session:$popup_window"
+  tmux display-popup -h 80% -w 80% -E "tmux attach-session -t $floating_session:$floating_window"
   exit 0
 fi
 
@@ -102,5 +106,53 @@ if [[ "$1" == "show-window-menu" ]]; then
     | awk 'BEGIN {ORS=" "} {print $2, NR, "\"select-window -t", $1 "\""}' \
     | xargs tmux display-menu -T "#[align=centre fg=green] tmux " -x C -y C
 
+  exit 0
+fi
+
+if [[ "$1" == "show-command-palette" ]]; then
+  tmux display-popup -h 53% -w 33% -E "$0 fill-command-palette"
+
+  cmd=$(cat /tmp/tmux_command_to_run)
+  rm -rf /tmp/tmux_command_to_run
+  eval "tmux $cmd"
+fi
+
+if [[ "$1" == "fill-command-palette" ]]; then
+  declare -A tmux_commands=(
+    ["Choose Session"]="run-shell '~/bin/tmux-commands.sh choose-session'"
+    ["Choose Window in Session"]="choose-tree -wf\"##{==:##{session_name},#{session_name}}\""
+    ["Choose Window"]="choose-tree -wZ"
+    ["Create New Session"]="command-prompt -p \"New Session:\" \"new-session -A -s '%%'\""
+    ["Create New Window"]="new-window -c \"#{pane_current_path}\""
+    ["Detach from tmux"]="detach"
+    ["Enter Copy Mode"]="copy-mode"
+    ["Kill Current Pane"]="confirm-before -p \"Kill pane?\" kill-pane"
+    ["Kill Current Window"]="confirm-before -p \"Kill window?\" kill-window"
+    ["Kill Session"]="run-shell '~/bin/tmux-commands.sh kill-session'"
+    ["Move Pane to New Window"]="break-pane -d"
+    ["Toggle Floating Terminal"]="run-shell '~/bin/tmux-commands.sh floating-terminal'"
+    ["Rename Session"]="command-prompt -p \"Rename session:\" \"rename-session '%%'\""
+    ["Rename Window"]="command-prompt -p \"Rename window:\" \"rename-window '%%'\""
+    ["Split Pane Across Middle"]="split-window -v -c \"#{pane_current_path}\""
+    ["Split Pane Down Middle"]="split-window -h -c \"#{pane_current_path}\""
+    ["Switch to Last Session"]="switch-client -l"
+    ["Switch to Last Window"]="last-window"
+  )
+
+  keys=(${(k)tmux_commands})
+  keys=(${(i)${(k)tmux_commands}})
+
+  selection=$(printf '%s\n' "${keys[@]}" |
+    fzf --reverse \
+        --layout=reverse \
+        --cycle \
+        --info=hidden \
+        --preview-window=hidden)
+
+  # Exit if no selection was made.
+  [[ -z "$selection" ]] && exit 0
+
+  # Write the selected command to a temporary file.
+  echo "${tmux_commands[$selection]}" > /tmp/tmux_command_to_run
   exit 0
 fi
